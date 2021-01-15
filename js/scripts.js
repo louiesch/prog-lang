673ed3dc37651d791f695875902f48e916ef43c0
Louie Schoenknecht <luisesch97@gmail.com>
2021-01-15 12:57:35 -0800

    begin with declaring functions in JS

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("form#questions").submit(function(event) {
+    const question1 = $("select#question1").val();
+    const question2 = $("select#question2").val();
+    const question3 = $("select#question3").val();
+    const question4 = $("select#question4").val();
+    const question5 = $("select#question5").val();
+    const question6 = $("select#question6").val();
+
+  })
+})
